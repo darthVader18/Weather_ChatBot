dd3b340295324c07d6799a5251ca810b19c6c1ac
Chaitanya Singh Bisht <csbwork18@gmail.com>
2021-05-26 15:53:35 +0530

    1

---
+++ weather.py
@@ -0,0 +1,9 @@
+import requests
+def Weather(city):
+    API_key = "Your API Key here"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    
+    Final_url = base_url + "appid=" + API_key + "&q=" + city + "&units=metric"
+    weather_data = requests.get(Final_url).json()
+    
+    return weather_data['main']
